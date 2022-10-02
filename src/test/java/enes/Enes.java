05be6872422cfbab1c10493e7513b11c17b9bf49
Your Name <yavuzzsonmezz@gmail.com>
2022-10-01 22:15:40 -0400

    nighty

--- src/test/java/enes/Enes.java
+++ src/test/java/enes/Enes.java
@@ -3,5 +3,6 @@ package enes;
 public class Enes {
     public static void main(String[] args) {
         System.out.println("hellorentacar");
+        System.out.println("cuneytabiwelcome");
     }
 }
