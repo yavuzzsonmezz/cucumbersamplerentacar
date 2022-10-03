bcda07a478d91dfe6472927a85382d7c3249e7b4
Your Name <yavuzzsonmezz@gmail.com>
2022-10-02 22:17:26 -0400

    eleven

--- src/test/java/utilities/Driver.java
+++ src/test/java/utilities/Driver.java
@@ -1,4 +1,10 @@
 package utilities;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.openqa.selenium.WebDriver;
+
 public class Driver {
-}
+    private Driver() {
+    }
+}    
