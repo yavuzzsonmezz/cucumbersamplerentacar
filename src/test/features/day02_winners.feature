b882f0f8a083b0d6f902021f76fd192ebbd7777c
yavuzzsonmezz <yavuzzsonmezz@gmail.com>
2022-10-05 22:59:46 -0400

    winners future file

---
+++ src/test/features/day02_winners.feature
@@ -0,0 +1,9 @@
+Feature: Winners
+  Scenario: day02_Test02_winners
+    Given user navigate to the page
+    When user clicks on Signup button
+    And user verifies New User Signup is visible
+    And user enters email
+    And user enters name
+    Then user enters on Signup button
+
---
+++ src/test/java/stepdefinitions/WinnersLoginStepDef.java
@@ -0,0 +1,35 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class WinnersLoginStepDef {
+
+    @Given("user navigate to the page")
+    public void user_navigate_to_the_page() {
+
+    }
+    @When("user clicks on Signup button")
+    public void user_clicks_on_signup_button() {
+
+    }
+    @When("user verifies New User Signup is visible")
+    public void user_verifies_new_user_signup_is_visible() {
+
+    }
+    @When("user enters email")
+    public void user_enters_email() {
+
+    }
+    @When("user enters name")
+    public void user_enters_name() {
+
+    }
+
+
+
+    @Then("user enters on Signup button")
+    public void userEntersOnSignupButton() {
+    }
+}
